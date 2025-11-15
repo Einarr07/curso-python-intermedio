60389fd8f7c0cea2886fb14e80727458176ba343
Einarr07 <aidenperse@gmail.com>
2025-11-14 19:07:21 -0500

    manual iteration control

--- app/example.py
+++
@@ -1,4 +0,0 @@
-import main
-
-if __name__ == '__main__':
-    print(f'This is the file main: {main.data}')
--- package/__init__.py
+++
@@ -1,6 +0,0 @@
-# if we work with a lower version that 3.3 this file is required
-# with version >= 3.3 is optional
-
-print('Package init')
-
-URL = 'URL FILE'
